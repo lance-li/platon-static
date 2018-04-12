3682ced3d3288392e536fceda0f7a2671764096a
三十文 <1047210087@qq.com>
2018-04-12 19:44:54 +0800

    1

--- src/modules/practice/components/DiscussTopBar/DiscussTopBar.tsx
+++ src/modules/practice/components/DiscussTopBar/DiscussTopBar.tsx
@@ -13,58 +13,18 @@ export default class DiscussTopBar extends React.Component<DiscussTopBarProps, a
     this.state = {}
   }
 
+  autoFixedTimer
+
   componentDidMount () {
     let node = this.refs.discussTopBar
-    let startX, startY, moveEndX, moveEndY, X, Y
-    let body = document.getElementsByTagName('body')[0]
-    if (node) {
-      body.addEventListener('touchstart', e => {
-        startX = e.touches[0].pageX
-        startY = e.touches[0].pageY
-      })
-      body.addEventListener('touchmove', (e) => {
-        moveEndX = e.changedTouches[0].pageX
-        moveEndY = e.changedTouches[0].pageY
-        X = moveEndX - startX
-        Y = moveEndY - startY
-        let rect = node.getBoundingClientRect()
-        if (Y > 0) {
-          node.classList.remove('fixed')
-        } else if (Y < 0) {
-          if (rect.top < 0) {
-            node.classList.add('fixed')
-          }
-        }
-      })
-      body.addEventListener('touchcancel', (e) => {
-        moveEndX = e.changedTouches[0].pageX
-        moveEndY = e.changedTouches[0].pageY
-        X = moveEndX - startX
-        Y = moveEndY - startY
-        let rect = node.getBoundingClientRect()
-        if (Y > 0) {
-          node.classList.remove('fixed')
-        } else if (Y < 0) {
-          if (rect.top < 0) {
-            node.classList.add('fixed')
-          }
-        }
-      })
-      body.addEventListener('scroll', (e) => {
-        moveEndX = e.changedTouches[0].pageX
-        moveEndY = e.changedTouches[0].pageY
-        X = moveEndX - startX
-        Y = moveEndY - startY
-        let rect = node.getBoundingClientRect()
-        if (Y > 0) {
-          node.classList.remove('fixed')
-        } else if (Y < 0) {
-          if (rect.top < 0) {
-            node.classList.add('fixed')
-          }
-        }
-      })
-    }
+    this.autoFixedTimer = setInterval(() => {
+      let rect = node.getBoundingClientRect()
+      if (rect.top < 0) {
+        node.classList.add('fixed')
+      } else {
+        node.classList.remove('fixed')
+      }
+    }, 500)
   }
 
   render () {
